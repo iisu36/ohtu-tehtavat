4f83cd779953a9ebc36f7f4cc4f18997d9060ac9
Iisakki Kulhua <iisakki@kulhua.net>
2021-11-15 09:37:42 +0200

    project-reader done

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -7,9 +7,11 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
+        import tomli
+
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
+        toml_dict = tomli.loads(content)['tool']['poetry']
+        
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(toml_dict['name'], toml_dict['description'], toml_dict['dependencies'], toml_dict['dev-dependencies'])
