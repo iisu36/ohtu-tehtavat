045f1e3156b6bd22ef676fb9a11a1a9089074bc8
Iisakki Kulhua <iisakki@kulhua.net>
2021-11-28 11:35:57 +0200

    tdd osa13 kahden saman tuotteen tapauksessa toisen poistaminen jättää jäljellä yhden. Lisäksi refaktoroitiin ostoskori listamuodosta sanakirjamuotoon

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,13 +3,13 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.kori = []
+        self.kori = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         tavarat = 0
 
-        for ostos in self.kori:
+        for ostos in self.kori.values():
             tavarat += ostos.lukumaara()
 
         return tavarat    
@@ -20,7 +20,7 @@ class Ostoskori:
     def hinta(self):
         hinta = 0
 
-        for ostos in self.kori:
+        for ostos in self.kori.values():
             hinta += ostos.hinta()
 
         return hinta
@@ -28,21 +28,23 @@ class Ostoskori:
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        for ostos in self.kori:
-            if ostos.tuotteen_nimi() == lisattava.nimi():
-                ostos.muuta_lukumaaraa(1)
-                break
-        else: self.kori.append(Ostos(lisattava))
+        ostos = self.kori.get(lisattava.nimi())
+        if ostos:
+            ostos.muuta_lukumaaraa(1)
+        else:
+            self.kori[lisattava.nimi()] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
-        pass
+        poistettava_tuote = self.kori.get(poistettava.nimi())
+        poistettava_tuote.muuta_lukumaaraa(-1)
+        if poistettava_tuote.lukumaara() == 0: self.kori.popitem(poistettava.nimi())
 
     def tyhjenna(self):
         pass
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        return self.kori
+        return list(self.kori.values())
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -98,4 +98,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 2)
 
-    
+    def test_jos_korissa_on_kaksi_samaa_tuotetta_ja_toinen_poistetaan_koriin_jaa_ostos_jossa_on_tuotetta_yksi_kappale(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 1)
