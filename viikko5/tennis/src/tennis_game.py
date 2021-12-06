5cf97aa8121f9a7bf76a573d64dafc14dfa0a100
Iisakki Kulhua <iisakki@kulhua.net>
2021-12-06 11:17:08 +0200

    osa3 oma metodi yli neljän pisteiden käsittelyyn

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -12,46 +12,53 @@ class TennisGame:
             self.player2_points = self.player2_points + 1
 
     def get_score(self):
-        score = ""
+        score_text = ""
         temp_score = 0
 
-        if self.player1_points == self.player2_points:
-            if self.player1_points == 0:
-                score = "Love-All"
-            elif self.player1_points == 1:
-                score = "Fifteen-All"
-            elif self.player1_points == 2:
-                score = "Thirty-All"
-            elif self.player1_points == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.player1_points >= 4 or self.player2_points >= 4:
-            minus_result = self.player1_points - self.player2_points
+        if self.player1_points >= 4 or self.player2_points >= 4: 
+            score_text =  self.score_for_four_and_higher()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
+        elif self.player1_points == self.player2_points:
+            if self.player1_points == 0:
+                score_text = "Love-All"
+            elif self.player1_points == 1:
+                score_text = "Fifteen-All"
+            elif self.player1_points == 2:
+                score_text = "Thirty-All"
+            elif self.player1_points == 3:
+                score_text = "Forty-All"
             else:
-                score = "Win for player2"
+                score_text = "Deuce"
+
         else:
             for i in range(1, 3):
                 if i == 1:
                     temp_score = self.player1_points
                 else:
-                    score = score + "-"
+                    score_text = score_text + "-"
                     temp_score = self.player2_points
 
                 if temp_score == 0:
-                    score = score + "Love"
+                    score_text = score_text + "Love"
                 elif temp_score == 1:
-                    score = score + "Fifteen"
+                    score_text = score_text + "Fifteen"
                 elif temp_score == 2:
-                    score = score + "Thirty"
+                    score_text = score_text + "Thirty"
                 elif temp_score == 3:
-                    score = score + "Forty"
+                    score_text = score_text + "Forty"
 
-        return score
+        return score_text
+
+    def score_for_four_and_higher(self):
+        difference_in_points = self.player1_points - self.player2_points
+
+        if difference_in_points < -2: difference_in_points = -2
+        if difference_in_points > 2: difference_in_points = 2
+
+        score_text_options = [f'Win for {self.player2_name}', 
+                                f'Advantage {self.player2_name}', 
+                                'Deuce', 
+                                f'Advantage {self.player1_name}', 
+                                f'Win for {self.player1_name}']
+
+        return score_text_options[difference_in_points + 2]
